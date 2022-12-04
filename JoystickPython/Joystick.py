3f57e2fe7247667b01274e6c930a19aa0fd03b33
Luis Miguel Medina <97616004+LuisMiguelMedina@users.noreply.github.com>
2022-12-04 11:01:28 -0600

    SerialAttempt

--- JoystickPython/Joystick.py
+++ JoystickPython/Joystick.py
@@ -1,26 +1,7 @@
-import serial.tools.list_ports
+import serial, time
+serialInst = serial.Serial('COM3', 9600)
+time.sleep(2)
+String = serialInst.readline()
+print(String)
 
-ports = serial.tools.list_ports.comports()
-serialInst = serial.Serial()
-
-portsList = []
-
-for onePort in ports:
-    portsList.append(str(onePort))
-    print(str(onePort))
-
-val = input("Select Port: COM")
-
-for x in range(0,len(portsList)):
-    if portsList[x].startswith("COM" + str(val)):
-        portVar = "COM" + str(val)
-        print(portVar)
-
-serialInst.baudrate = 9600
-serialInst.port = portVar
-serialInst.open()
-
-while True:
-	if serialInst.in_waiting:
-		packet = serialInst.readline()
-		print(packet.decode('utf').rstrip('\n'))
+serialInst.close()
